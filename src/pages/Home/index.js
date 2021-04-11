b3c9b6888a0efdf1a20c07a85305221886dbaa2f
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-11 15:01:10 +0300

    update homePage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Container} from "semantic-ui-react";
 import PostsList from "../../components/PostsList";
+import { getAllPost } from "../../actions/post";
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import Preloader from "../../components/Preloader";
 import './styles.scss';
 
-const Home = () => {
+const Home = ({
+    getAllPost,
+    posts
+}) => {
+    useEffect(() => {
+        getAllPost();
+    }, []);
+
+    if (!posts) {
+        return <Preloader />
+    }
+
     return (
         <div className="page">
             <Container className="HomePage">
                 <h2>Posts</h2>
-                <PostsList />
+                <PostsList posts={posts} />
             </Container>
         </div>
     );
 };
 
-export default Home;
+Home.propTypes = {
+    getAllPost: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired
+};
+
+const actions = {
+    getAllPost
+};
+
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+const mapStateToProps = ({ post }) => ({
+   posts: post
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
